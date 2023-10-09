a96d214bf3073545c7814d7ce0d31faec52d359d
Nick-Siska <87763871+Nick-Siska@users.noreply.github.com>
2023-10-09 18:48:48 +0100

    New folder containing c script for practical 03 and a README.md file

---
+++ practical03/Area_Under_Curve.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  // Initializing my variables and setting their respective values
+  double a = 0.0; 
+  double b = M_PI/3;
+  int N = 12;
+  double sum = 0.0;
+  double result = 0.0;
+  double xi = 0.0;
+  double log2_result = log(2.0);
+  // Creating a for loop that will calculate the 2*tan(xi) and save it in sum
+  for(int i = 0;i<= N;i++){
+    xi = a +i*(b-a)/N;
+    sum = sum + 2.0*tan(xi);
+    //printf("xi is equalt to:  %.2f\n", xi);
+    //printf("Sum is equalt to:  %.2f\n",sum);
+  }
+  // Adding the tan of the endpoints plus the previously calculated sum.
+  // This is multipled what is specified in the formula
+  result = (tan(a) + tan(b) +sum)*(b - a)/(2.0*N);
+  printf("End Result:  %.4f\n", result);
+  printf(" Log2 Result: %.4f \n",log2_result);
+
+
+}
---
+++ practical03/README.md
@@ -0,0 +1,3 @@
+This is for the practical03
+gcc -o myprogram Area_under_Curve.c -lm
+/.myprogram
