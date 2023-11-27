c0ae2e89256f10fe6bcbbe37f0ec7658813ff6a3
Nick-Siska <87763871+Nick-Siska@users.noreply.github.com>
2023-11-27 23:57:34 +0000

    Adding practical05 which arctan.c and fibonnaci scripts

---
+++ practical05/arctan.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#inlcude<math.h>
+
+double arctanh1(const double x, const double delta);
+double arctanh2(const double x);
+
+int main(){
+  double delta, x;
+  printf("Enter the precision for the Maclauren Series: \n");
+  scanf("%lf",&delta);
+  int length = 1000;
+  double tan1[length]; // Storing the result of arctan1
+  double tan2[length]; // Storing the result of arctan2
+  int j = 0;
+  x= -0.9;
+  while(x<= 0.9 && j < length){
+    tan1[j] = arctanh1(x,delta);
+    tan2[j] = arctanh2(x);
+    printf("the difference at x= %lf between them is %.10lf \n ",x, fabs(tan1[j] - tan2[j]));
+    j ++;
+    x+ = 0.1;
+  }
+
+
+  return 0;
+}
+
+double arctanh1(const double x, const double delta){
+  double arcTan = 0; // Approximation to the solution
+  double elem, val; //element in the series
+  int n = 0; //sum parameter
+  do{
+    val = 2*n+1;
+    elem = pow(x,val)/val;
+    arcTan += elem;
+    n++;
+
+  }while(fabs(elem)>= delta);
+  return arcTan;
+
+}
+// Add error checking
+double arctanh2(const double x){
+  return(log(1 + x) log(1 - x))/2;
+}
